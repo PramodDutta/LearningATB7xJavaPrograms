d9c481b82e1ec59c23d4968f48ee7c30bb316c8f
Promode <pramoddutta_96@yahoo.com>
2024-08-10 08:37:02 +0530

    4th aug part 2

---
+++ src/aug/ex_04082024/FatherElon.java
@@ -0,0 +1,9 @@
+package aug.ex_04082024;
+
+public class FatherElon {
+    class Son1{
+        class Son2{
+
+        }
+    }
+}
---
+++ src/aug/ex_04082024/Lab177.java
@@ -0,0 +1,14 @@
+package aug.ex_04082024;
+
+public class Lab177 {
+
+    class Pramod{
+        class Lucky{
+            class AMit{
+                class Shardha{
+
+                }
+            }
+        }
+    }
+}
